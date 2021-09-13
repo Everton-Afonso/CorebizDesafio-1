4ffa52058ac408defa39801fbe6b4e8f27ce22d8
Everton Otavio Afonso <evertonafonso0@gmail.com>
2021-09-13 17:39:37 -0300

    NewsDesk sucesso desktop completed

--- src/components/NewsDesk/index.jsx
+++ src/components/NewsDesk/index.jsx
@@ -1,25 +1,90 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 import "./styles.css";
 
-const NewsDesk = () => {
-  return (
-    <div className="news">
-      <aside>
-        <h1>Participe de nossas news com promoções e nocidades!</h1>
-      </aside>
-      <main className="main-content">
-        <form className="form-news">
-          <input type="text" placeholder="Digite seu nome" />
-          <input type="email" placeholder="Digite seu email" />
+function NewsDesk() {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [account, setAccount] = useState([]);
 
-          <div className="container-button-news">
-            <button className="button-news button--flex">Eu quero!</button>
-          </div>
-        </form>
-      </main>
-    </div>
+  const isNull = localStorage.getItem("clients");
+
+  useEffect(() => {
+    const clients = JSON.parse(localStorage.getItem("clients"));
+    if (clients) {
+      setAccount(clients);
+    }
+  }, []);
+
+  function handleRegister(e) {
+    e.preventDefault();
+
+    const data = {
+      name,
+      email,
+    };
+
+    const newAccount = JSON.stringify([...(account || []), data]);
+    localStorage.setItem("clients", newAccount);
+    window.location.reload();
+  }
+
+  function handleNewRegister(e) {
+    e.preventDefault();
+    localStorage.removeItem("clients");
+    window.location.reload();
+  }
+  return (
+    <>
+      {isNull === null ? (
+        <section className="news">
+          <aside>
+            <h1>Participe de nossas news com promoções e nocidades!</h1>
+          </aside>
+          <main className="main-content">
+            <form className="form-news" onSubmit={handleRegister}>
+              <input
+                type="text"
+                value={name}
+                placeholder="Digite seu nome"
+                onChange={(e) => setName(e.target.value)}
+                required
+              />
+              <input
+                type="email"
+                value={email}
+                placeholder="Digite seu email"
+                onChange={(e) => setEmail(e.target.value)}
+                required
+              />
+
+              <div className="container-button-news">
+                <button className="button-news button--flex">Eu quero!</button>
+              </div>
+            </form>
+          </main>
+        </section>
+      ) : (
+        <section className="news">
+          <section>
+            <h1>Seu e-mail foi cadastrado com sucesso!</h1>
+            <p>
+              a partir de agora você receberá as novidade e ofertas exclusivas
+            </p>
+
+            <div className="container-button-news-register">
+              <button
+                className="button-success button--flex"
+                onClick={handleNewRegister}
+              >
+                Cadastrar novo e-mail
+              </button>
+            </div>
+          </section>
+        </section>
+      )}
+    </>
   );
-};
+}
 
 export default NewsDesk;
